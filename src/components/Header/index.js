10eeb8c98c25400443740684a17db7f3574ee7a0
Yan Batista <ybatistasouza@hotmail.com>
2022-03-18 19:45:36 -0300

    react switch syntax correction

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,7 +14,7 @@ const Header = props => {
                 onChange={props.toggleTheme}
                 checked={title === 'dark'}
                 checkedIcon={false}
-                unchekedIcon={false}
+                uncheckedIcon={false}
                 height={10}
                 width={20}
             />
